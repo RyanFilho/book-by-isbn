0de393b3492cbac7f37ecb313e20e49ce721422a
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2015-10-18 17:32:07 -0300

    Book View

--- source/js/controllers/bookController.js
+++ source/js/controllers/bookController.js
@@ -2,6 +2,13 @@ app.controller('bookController', ['$scope', '$routeParams', 'bookSearchService',
 	var isbn = $routeParams.isbn;
 	bookSearch = bookSearchService.searchBook;
 	bookSearch(isbn, function (response) {
-		console.log(response);
+		$scope.book = {};
+		$scope.book.title = response.title;
+		$scope.book.authors = response.authors.join(',');
+		$scope.book.publishedDate = response.publishedDate;
+		$scope.book.pageCount = response.pageCount;
+		$scope.book.description = response.description;
+		$scope.book.industryIdentifiers = [response.industryIdentifiers[0].identifier, response.industryIdentifiers[1].identifier];
+
 	});
 }]);
