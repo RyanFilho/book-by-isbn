f928c1bfca368f49d751cdfe4a3926a771b9fd53
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2017-12-31 15:41:07 -0300

    Change to React.js

--- README.md
+++
@@ -1,6 +0,0 @@
-# Book by ISBN
-
-Angular.js application to look for books by ISBN on public APIs:
-
-- Google Books API (Coming soon)
-- Open Library (Coming soon)
--- source/js/controllers/bookController.js
+++
@@ -1,9 +0,0 @@
-app.controller('bookController', ['$scope', '$routeParams', 'bookSearchService', 'bookDataProcessService', function ($scope, $routeParams, bookSearchService, bookDataProcessService) {
-	var isbn = $routeParams.isbn;
-	bookSearch = bookSearchService.searchBook;
-	bookDataProcess = bookDataProcessService.processData;
-	bookSearch(isbn, function (response) {
-		$scope.book = bookDataProcess(response);
-	});
-
-}]);
